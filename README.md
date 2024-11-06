9ffa5f97405cffb9ef4714607b1301e580f8c86b
Morgan <morno368@student.liu.se>
2024-11-06 19:05:44 +0100

    Updated readme and did some more work on the emulator itself

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CHIP-8
+
+
+# HELPFUL RESOURCES
+- https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,15 @@ uint8_t* memory[4096];
 // white
 
 bool** display[32][64];
-
-// A program counter, often called just “programCounter”, which points at the current
-// instruction in memory
+// A program counter, often called just “programCounter”, which points at the
+// current instruction in memory
+//
+// The first CHIP-8 interpreter (on the COSMAC VIP computer) was also located in
+// RAM, from address 000 to 1FF. It would expect a CHIP-8 program to be loaded
+// into memory after it, starting at address 200 (512 in decimal). Although
+// modern interpreters are not in the same memory space, you should do the same
+// to be able to run the old programs; you can just leave the initial space
+// empty, except for the font.
 uint16_t programCounter;
 
 // One 16-bit index register called “I” which is used to point at locations in
@@ -69,24 +75,26 @@ uint8_t VD;
 uint8_t VE;
 uint8_t VF;
 
+
 // FONT
-//
-// 0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-// 0x20, 0x60, 0x20, 0x20, 0x70, // 1
-// 0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-// 0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-// 0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-// 0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-// 0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-// 0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-// 0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-// 0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-// 0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-// 0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-// 0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-// 0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-// 0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-// 0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+const uint8_t FONT_SET[80] = {
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 
 // KEYPAD
@@ -94,7 +102,6 @@ uint8_t VF;
 // Q 	W 	E 	R
 // A 	S 	D 	F
 // Z 	X 	C 	V
-
 
 
 void decrementTimers() {
@@ -108,45 +115,62 @@ void decrementTimers() {
 
 
 void updateDisplay() {
+	BeginDrawing();
 	ClearBackground(BLACK);
+	
+	for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+		for (int x = 0; x < DISPLAY_WIDTH; x++) {
+			if (display[y][x]) {
+				DrawRectangle(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, WHITE);
+			}
+		}
+	}
 
-	for (int y = 0; y < DISPLAY_HEIGHT; y++)
+	EndDrawing();
 }
 
 
+// Fetch the instruction from memory at the current programCounter (program
+// counter)
 const uint16_t fetch() {
 	uint8_t d1 = *memory[programCounter];
 	programCounter++;
 	uint8_t d2 = *memory[programCounter];
 	programCounter++;
-	uint16_t wd = (d2 << 8) | d1;
+	uint16_t wd = (d2 << 8) | d1; //bitshift 8 and bitwise OR 
 	return wd;
 }
 
 
+// Decode the instruction to find out what the emulator should do
+void decode(const uint16_t& instruction) {
+	// X: The second nibble. Used to look up one of the 16 registers (VX) from V0 through VF.
+	// Y: The third nibble. Also used to look up one of the 16 registers (VY) from V0 through VF.
+	// N: The fourth nibble. A 4-bit number.
+	// NN: The second byte (third and fourth nibbles). An 8-bit immediate number.
+	// NNN: The second, third and fourth nibbles. A 12-bit immediate memory address.
+	
+	// switch() {
+
+	// }
+
+
+}
+
 
 int main() {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "CHIP-8 EMULATOR");
-	
 	SetTargetFPS(FRAMERATE);
-
+	
 	while(!WindowShouldClose()) {
-		
 		decrementTimers();
-
-		// Fetch the instruction from memory at the current programCounter (program
-		// counter)
-
-		// Decode the instruction to find out what the emulator should
-		// do
-
-		// Execute the instruction and do what it tells you
-
-		BeginDrawing();
+		updateDisplay();
 		
-		ClearBackground(BLACK);
+		// Fetch -> Decode -> Execute
+		// const uint16_t instruction = fetch();
+		// decode(instruction);
+		// execute(instruction); // just do in decode-switch
 
-		EndDrawing();
 	}
 
 	CloseWindow();
